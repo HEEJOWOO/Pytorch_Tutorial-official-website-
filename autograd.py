3ea772551a1113d6b6f339fe11752fd2cc8c144c
HEEJO WOO <61686244+HEEJOWOO@users.noreply.github.com>
2021-06-19 15:20:43 +0900

    Add files via upload

---
+++ autograd.py
@@ -0,0 +1,14 @@
+import torch
+
+x = torch.ones(5)
+print(x)
+y = torch.zeros(3)
+print(y)
+w = torch.randn(5,3,requires_grad=True)
+b = torch.randn(3,requires_grad=True)
+z = torch.matmul(x,w)+b
+loss = torch.nn.functional.binary_cross_entropy_with_logits(z,y)
+
+loss.backward()
+print(w.grad)
+print(b.grad)
